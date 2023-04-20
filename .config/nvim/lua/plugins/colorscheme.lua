0fd671e430e0701306552a53ac1e0a86662fb6d4
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-04-20 03:14:56 +0300

    Update dotfiles

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-  -- Add colorschemes
-  { "shaunsingh/nord.nvim" },
-  { "rose-pine/nvim" },
-  { "folke/tokyonight.nvim" },
+    -- Add colorschemes
+    { "shaunsingh/nord.nvim" },
+    { "rose-pine/nvim" },
+    { "folke/tokyonight.nvim" },
 
-  -- Set colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
+    -- Set colorscheme
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "rose-pine",
+        },
     },
-  },
 }
