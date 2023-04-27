30db194f1f50514cb581ec21edc2c92a100b5c3a
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-04-27 17:21:17 +0300

    Update alpha.lua

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -8,11 +8,8 @@ return {
 ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-                [ @whitez ]
+                 [ @whitez ]
     ]]
         dashboard.section.header.val = vim.split(logo, "\n", { trimempty = true })
-        local button = dashboard.button("p", " " .. " Projects", ":Telescope projects <CR>")
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
     end,
 }
