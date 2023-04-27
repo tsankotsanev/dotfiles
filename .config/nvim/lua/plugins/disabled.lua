bc5672ddc53dc3a37c37ba081ceaad3276fdb24b
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-04-27 17:21:36 +0300

    Enable nvim.notify

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,6 @@
 return {
     { "akinsho/bufferline.nvim", enabled = false },
     -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-    { "rcarriga/nvim-notify", enabled = false },
+    -- { "rcarriga/nvim-notify", enabled = false },
     -- { "folke/noice.nvim", enabled = false },
 }
