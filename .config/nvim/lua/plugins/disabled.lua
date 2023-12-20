07491bb008025ddeb59a485a4d260baa66b6ed07
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2023-12-20 12:25:41 +0200

    Update dotfiles

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,7 +4,7 @@ return {
     { "jackMort/ChatGPT.nvim", enabled = false },
     { "b0o/SchemaStore.nvim", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-    { "lukas-reineke/indent-blankline.nvim", enabled = false },
+    -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
     -- { "nvim-lualine/lualine.nvim", enabled = false },
     -- { "rcarriga/nvim-notify", enabled = false },
     -- { "folke/noice.nvim", enabled = false },
