fa695bdc495270f36a7715070df71cd8fca73e2c
Tsanko Tsanev <tsanko.tsanev.business@gmail.com>
2026-06-12 23:50:22 +0300

    chore: adopt local machine configs via stow --adopt
    Adopted current live configs from this machine into the stow packages:
    - alacritty: local alacritty.toml changes
    - fish: updated completions, config.fish
    - git: local .gitconfig
    - nvim: lazy-lock.json, lazy.lua, colorscheme.lua updates
    - tmux: local .tmux.conf changes

--- packages/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ packages/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
--- Shared Neovim config stays theme-neutral.
---
--- Activate a machine/theme-specific package instead:
---   stow --dir packages --target "$HOME" --no-folding --restow nvim-catppuccin
---   stow --dir packages --target "$HOME" --no-folding --restow nvim-onedark
---
--- Those packages provide lua/plugins/colorscheme.local.lua and set both the
--- editor colorscheme and vim.g.dotfiles_lualine_theme.
+-- return {
+--     "LazyVim/LazyVim",
+--     opts = {
+--         colorscheme = "catppuccin",
+--         lazy = true,
+--     },
+-- }
 
-return {}
+return {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        require("onedark").setup({
+            style = "dark",
+        })
+        require("onedark").load()
+    end,
+}
